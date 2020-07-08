6c8fc72f5aecf524f71c10468fbda5c31ed20517
junghwa1996 <59001439+junghwa1996@users.noreply.github.com>
2020-07-08 23:29:44 +0900

    Update fixd.js

--- RRR/js/fixd.js
+++ RRR/js/fixd.js
@@ -1,6 +1,8 @@
 $(function(){
     searchopen();
     navFixed();
+    quickFixed();
+    scrollTopbtn();
 });
 
 function searchopen(){
@@ -21,5 +23,30 @@ function navFixed(){
         }else{
             $('.Fixd_Nav').removeClass('fixed');
         }
+    });
+}
+
+
+function quickFixed(){
+    var nav = $('.header_Wrap').height();
+    $(window).scroll(function(){
+        if($(this).scrollTop() > nav){
+            $('.quick_menu_Left').addClass('quickfixed');
+        }else{
+            $('.quick_menu_Left').removeClass('quickfixed');
+        }
+    });
+    $(window).scroll(function(){
+        if($(this).scrollTop() > nav){
+            $('.quick_menu_right').addClass('quickfixed_right');
+        }else{
+            $('.quick_menu_right').removeClass('quickfixed_right');
+        }
+    });
+}
+
+function scrollTopbtn(){
+    $('.scrollTop').on('click',function(){
+        $('html,body').stop().animate({scrollTop:0},1000);
     });
 }

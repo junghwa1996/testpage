0b87f789c1de2f2a692565b37bd69c52a53e02c3
junghwa1996 <59001439+junghwa1996@users.noreply.github.com>
2020-07-28 03:21:43 +0900

    Add files via upload

---
+++ Web_Naver_login/main.js
@@ -0,0 +1,17 @@
+// $(function(){
+
+//     $id = $('#IdInput');
+
+//     $id.keypress(function (e) { 
+//         if (e.which == 13) {
+//             $span = $('.id > span');
+//             if($id.val()=='') {
+//                   $span.css('display','block');
+//             }
+//             else{ $id.focus();} 
+//         } 
+//     });
+
+
+// });
+
